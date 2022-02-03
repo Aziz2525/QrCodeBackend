7e8e666fcb9c9048da852c33f358e93f0a00b492
Aziz2525 <aziz.app.developer@gmail.com>
2022-02-03 22:44:44 +0300

    make it better

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const https = require('https');
 const app = express();
 
 const cors = require("cors");
-const fs = require('fs');
 require("dotenv/config");
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/apithon.com.tr/privkey.pem', 'utf8')
-const certificate = fs.readFileSync('/etc/letsencrypt/live/apithon.com.tr/fullchain.pem', 'utf8')
-const credentials = {
-   key: privateKey,
-   cert: certificate
-}
+
 
 //routers
 const generate = require("./router/generate");
@@ -25,7 +19,7 @@ app.use(`/`, generate);
 app.use(`/`, barcode);
 
 
-https.createServer(credentials, app).listen(process.env.PORT || 80, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('API listening on HTTPS port ' + port + '.');
  });
  
